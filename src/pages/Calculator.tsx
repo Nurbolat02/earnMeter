ed562333f68b8ea9e3ffcea8247c95aae439dd7d
Айдильдин Нурболат <Nurbolat@MBP-mac-4.fritz.box>
2026-06-18 18:47:45 +0200

    try one more time

--- src/pages/Calculator.tsx
+++ src/pages/Calculator.tsx
@@ -1,29 +1,14 @@
-import { EarnMeterForm } from "../components/EarnMeterForm";
-import { ResultDisplay } from "../components/ResultDisplay";
-import { useEarnMeter } from "../hooks/useEarnMeter";
-import { Link } from "react-router-dom";
 import classes from "./Calculator.module.css";
 
 export default function Calculator() {
-  const earnMeter = useEarnMeter();
-
   return (
     <div className={classes.card}>
       <h1 className={classes.title}>EarnMeter</h1>
       <p className={classes.subtitle}>
         Calculate how many hours you need to work
       </p>
-      <EarnMeterForm {...earnMeter} />
 
-      <div className={classes.result}>
-        <ResultDisplay {...earnMeter} />
-
-        {earnMeter.isCalculated && (
-          <Link className={classes.button} to="/planer">
-            Plan shifts
-          </Link>
-        )}
-      </div>
+      <div className={classes.result}></div>
     </div>
   );
 }
